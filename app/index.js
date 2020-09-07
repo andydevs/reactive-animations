2d89e7778d266b3b64089548db57728f502ea563
Anshul Kharbanda <akanshul97@gmail.com>
2020-09-07 03:20:44 -0400

    Created click observers

--- app/index.js
+++ app/index.js
@@ -1,46 +1,36 @@
 /**
- * Use this template for building basic static websites
+ * Using rxjs to animate javascript components
  * 
- * Author:  [Your Name Here]
- * Created: [Date of Creation]
+ * Author:  Anshul Kharbanda
+ * Created: 9 - 7 - 2020
  */
 import './style/main.scss'
+import * as vector from './vector';
 import { fromEvent, of, animationFrameScheduler } from 'rxjs'
 import { map, repeat } from 'rxjs/operators';
 
 // Speed Factor
 const speedFactor = 0.25
 
+// Capture mousepos
+let mousepos = {}
+
 // Get app
 let app = document.getElementById('app')
 
 // Create redbox
 let redbox = document.createElement('div')
+let redClickObserver$ = fromEvent(redbox, 'click')
+let redboxAnimationSubscriber = null
 redbox.classList.add('redbox')
 app.appendChild(redbox)
 
 // Create bluebox
 let bluebox = document.createElement('div')
+let blueClickObserver$ = fromEvent(bluebox, 'click')
+let blueboxAnimationSubscriber = null
 bluebox.classList.add('bluebox')
 app.appendChild(bluebox)
-
-// Capture mousepos
-let mousepos = {}
-
-// Pixel value from string
-const pixVal = val => val === '' ? 0 : parseInt(val)
-
-// Vector distance
-const distance = (a, b) => ({ x: b.x - a.x, y: b.y - a.y })
-
-// Vector addition
-const add = (a, b) => ({ x: a.x + b.x, y: a.y + b.y })
-
-// Vector scale
-const scale = (a, s) => ({ x: a.x * s, y: a.y * s })
-
-// Vector floor
-const vfloor = a => ({ x: Math.floor(a.x), y: Math.floor(a.y) })
 
 // Animation observable
 let animationFrames$ = of(null, animationFrameScheduler)
@@ -53,32 +43,46 @@ let mousePos$ = fromEvent(document, 'mousemove')
     .pipe(
         map(event => ({ x: event.clientX, y: event.clientY }))
     )
-
-// Move red box
-mousePos$.subscribe(pos => {
-    redbox.style.left = pos.x + 'px'
-    redbox.style.top = pos.y + 'px'
-})
-
+    
 // Set global mousepos
 mousePos$.subscribe(pos => {
     mousepos = pos
 })
 
-// Move bluebox
-animationFrames$.subscribe(() => {
-    // Get position
-    let current = {}
-    current.x = pixVal(bluebox.style.left)
-    current.y = pixVal(bluebox.style.top)
+redClickObserver$.subscribe(event => {
+    if (redboxAnimationSubscriber) {
+        redboxAnimationSubscriber.unsubscribe()
+        redboxAnimationSubscriber = null
+    }
+    else {
+        redboxAnimationSubscriber = animationFrames$.subscribe(() => {
+            redbox.style.left = mousepos.x + 'px'
+            redbox.style.top = mousepos.y + 'px'
+        })
+    }
+})
 
-    // Get distance to mousepos and scale distance down
-    let dist = distance(current, mousepos)
-    let velocity = scale(dist, speedFactor)
-    let next = add(current, velocity)
-    next = vfloor(next)
+blueClickObserver$.subscribe(event => {
+    if (blueboxAnimationSubscriber) {
+        blueboxAnimationSubscriber.unsubscribe()
+        blueboxAnimationSubscriber = null
+    }
+    else {
+        blueboxAnimationSubscriber = animationFrames$.subscribe(() => {
+            // Get position
+            let current = {}
+            current.x = vector.pixVal(bluebox.style.left)
+            current.y = vector.pixVal(bluebox.style.top)
 
-    // Set position
-    bluebox.style.left = next.x + 'px'
-    bluebox.style.top = next.y + 'px'
+            // Get distance to mousepos and scale distance down
+            let dist = vector.distance(current, mousepos)
+            let velocity = vector.scale(dist, speedFactor)
+            let next = vector.add(current, velocity)
+            next = vector.vfloor(next)
+
+            // Set position
+            bluebox.style.left = next.x + 'px'
+            bluebox.style.top = next.y + 'px'
+        })
+    }
 })
