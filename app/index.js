c187d180f7ec293b548bd0008209ce856bdeba89
Anshul Kharbanda <akanshul97@gmail.com>
2020-09-07 03:02:33 -0400

    Use animation frame observable

--- app/index.js
+++ app/index.js
@@ -5,8 +5,11 @@
  * Created: [Date of Creation]
  */
 import './style/main.scss'
-import { fromEvent } from 'rxjs'
-import { map } from 'rxjs/operators';
+import { fromEvent, of, animationFrameScheduler } from 'rxjs'
+import { map, repeat } from 'rxjs/operators';
+
+// Speed Factor
+const speedFactor = 0.25
 
 // Get app
 let app = document.getElementById('app')
@@ -17,10 +20,12 @@ redbox.classList.add('redbox')
 app.appendChild(redbox)
 
 // Create bluebox
-let mousepos = {}
 let bluebox = document.createElement('div')
 bluebox.classList.add('bluebox')
 app.appendChild(bluebox)
+
+// Capture mousepos
+let mousepos = {}
 
 // Pixel value from string
 const pixVal = val => val === '' ? 0 : parseInt(val)
@@ -32,10 +37,16 @@ const distance = (a, b) => ({ x: b.x - a.x, y: b.y - a.y })
 const add = (a, b) => ({ x: a.x + b.x, y: a.y + b.y })
 
 // Vector scale
-const scale = (a, s) => ({ 
-    x: Math.floor(a.x * s), 
-    y: Math.floor(a.y * s)
-})
+const scale = (a, s) => ({ x: a.x * s, y: a.y * s })
+
+// Vector floor
+const vfloor = a => ({ x: Math.floor(a.x), y: Math.floor(a.y) })
+
+// Animation observable
+let animationFrames$ = of(null, animationFrameScheduler)
+    .pipe(
+        repeat()
+    )
 
 // Mouse position observable
 let mousePos$ = fromEvent(document, 'mousemove')
@@ -54,7 +65,8 @@ mousePos$.subscribe(pos => {
     mousepos = pos
 })
 
-function animateBlueBox() {
+// Move bluebox
+animationFrames$.subscribe(() => {
     // Get position
     let current = {}
     current.x = pixVal(bluebox.style.left)
@@ -62,14 +74,11 @@ function animateBlueBox() {
 
     // Get distance to mousepos and scale distance down
     let dist = distance(current, mousepos)
-    let velocity = scale(dist, 0.02)
+    let velocity = scale(dist, speedFactor)
     let next = add(current, velocity)
+    next = vfloor(next)
 
     // Set position
     bluebox.style.left = next.x + 'px'
     bluebox.style.top = next.y + 'px'
-
-    // Request next animation frame
-    requestAnimationFrame(animateBlueBox)
-}
-requestAnimationFrame(animateBlueBox)
+})
