4386de8762404b5663a7a63ac728175e26f34ca1
Anshul Kharbanda <akanshul97@gmail.com>
2020-09-07 03:26:06 -0400

    Created separate mouse position variables

--- app/index.js
+++ app/index.js
@@ -9,29 +9,6 @@ import * as vector from './vector';
 import { fromEvent, of, animationFrameScheduler } from 'rxjs'
 import { map, repeat } from 'rxjs/operators';
 
-// Speed Factor
-const speedFactor = 0.25
-
-// Capture mousepos
-let mousepos = {}
-
-// Get app
-let app = document.getElementById('app')
-
-// Create redbox
-let redbox = document.createElement('div')
-let redClickObserver$ = fromEvent(redbox, 'click')
-let redboxAnimationSubscriber = null
-redbox.classList.add('redbox')
-app.appendChild(redbox)
-
-// Create bluebox
-let bluebox = document.createElement('div')
-let blueClickObserver$ = fromEvent(bluebox, 'click')
-let blueboxAnimationSubscriber = null
-bluebox.classList.add('bluebox')
-app.appendChild(bluebox)
-
 // Animation observable
 let animationFrames$ = of(null, animationFrameScheduler)
     .pipe(
@@ -43,31 +20,72 @@ let mousePos$ = fromEvent(document, 'mousemove')
     .pipe(
         map(event => ({ x: event.clientX, y: event.clientY }))
     )
-    
-// Set global mousepos
-mousePos$.subscribe(pos => {
-    mousepos = pos
-})
+
+// Speed Factor
+const speedFactor = 0.25
+
+// Get app
+let app = document.getElementById('app')
+
+// Create redbox
+let redbox = document.createElement('div')
+let redClickObserver$ = fromEvent(redbox, 'click')
+let redboxAnimationSubscriber = null
+let redboxMotionSubscriber = null
+let redboxMousePos = {}
+redbox.classList.add('redbox')
+app.appendChild(redbox)
+
+// Create bluebox
+let bluebox = document.createElement('div')
+let blueClickObserver$ = fromEvent(bluebox, 'click')
+let blueboxAnimationSubscriber = null
+let blueboxMotionSubscriber = null
+let blueboxMousePos = {}
+bluebox.classList.add('bluebox')
+app.appendChild(bluebox)
 
 redClickObserver$.subscribe(event => {
-    if (redboxAnimationSubscriber) {
-        redboxAnimationSubscriber.unsubscribe()
-        redboxAnimationSubscriber = null
+    if (redboxAnimationSubscriber || redboxMotionSubscriber) {
+        if (redboxMotionSubscriber) {
+            redboxMotionSubscriber.unsubscribe()
+            redboxMotionSubscriber = null
+        }
+
+        if (redboxAnimationSubscriber) {
+            redboxAnimationSubscriber.unsubscribe()
+            redboxAnimationSubscriber = null
+        }
     }
     else {
+        redboxMotionSubscriber = mousePos$.subscribe(pos => {
+            redboxMousePos = pos
+        })
+
         redboxAnimationSubscriber = animationFrames$.subscribe(() => {
-            redbox.style.left = mousepos.x + 'px'
-            redbox.style.top = mousepos.y + 'px'
+            redbox.style.left = redboxMousePos.x + 'px'
+            redbox.style.top = redboxMousePos.y + 'px'
         })
     }
 })
 
 blueClickObserver$.subscribe(event => {
-    if (blueboxAnimationSubscriber) {
-        blueboxAnimationSubscriber.unsubscribe()
-        blueboxAnimationSubscriber = null
+    if (blueboxAnimationSubscriber || blueboxMotionSubscriber) {
+        if (blueboxMotionSubscriber) {
+            blueboxMotionSubscriber.unsubscribe()
+            blueboxMotionSubscriber = null
+        }
+
+        if (blueboxAnimationSubscriber) {
+            blueboxAnimationSubscriber.unsubscribe()
+            blueboxAnimationSubscriber = null
+        }
     }
     else {
+        blueboxMotionSubscriber = mousePos$.subscribe(pos => {
+            blueboxMousePos = pos
+        })
+
         blueboxAnimationSubscriber = animationFrames$.subscribe(() => {
             // Get position
             let current = {}
@@ -75,7 +93,7 @@ blueClickObserver$.subscribe(event => {
             current.y = vector.pixVal(bluebox.style.top)
 
             // Get distance to mousepos and scale distance down
-            let dist = vector.distance(current, mousepos)
+            let dist = vector.distance(current, blueboxMousePos)
             let velocity = vector.scale(dist, speedFactor)
             let next = vector.add(current, velocity)
             next = vector.vfloor(next)
