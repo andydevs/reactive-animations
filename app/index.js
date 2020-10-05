2b047fcd5a8b95b998d6a8869110bda9ded563de
Anshul Kharbanda <akanshul97@gmail.com>
2020-10-04 23:21:51 -0400

    Moved ui code to separate file

--- app/index.js
+++ app/index.js
@@ -5,62 +5,18 @@
  * Created: 9 - 7 - 2020
  */
 import './style/main.scss'
-import { toggleSubscription, interpolate } from './operations';
-import { mousePos$ } from './observable';
-import { fromEvent } from 'rxjs';
-
-// Get app
-const app = document.getElementById('app')
-
-/**
- * Moves box to new position
- * 
- * @param {HTMLElement} box box to move
- * @param {{x: float, y: float}} pos new position of box 
- */
-function moveBox(box, pos) {
-    box.style.left = pos.x + 'px'
-    box.style.top = pos.y + 'px'
-}
-
-/**
- * 
- * @param {string} color color of box
- * @param {{x: float, y: float}} initial initial position
- */
-function createBox(color, initial={x: 0, y: 0}) {
-    let box = document.createElement('div')
-    box.classList.add('animated-box')
-    box.style.backgroundColor = color
-    app.appendChild(box)
-    moveBox(box, initial)
-    return box
-}
-
-function clickAndDrag(box) {
-    return fromEvent(box, 'click').subscribe(
-        toggleSubscription(
-            mousePos$,
-            pos => moveBox(box, pos)
-        )
-    )
-}
-
-function clickAndDragInterpolated(box, alpha) {
-    return fromEvent(box, 'click').subscribe(
-        toggleSubscription(
-            interpolate(alpha)(mousePos$),
-            pos => moveBox(box, pos)
-        )
-    )
-}
+import {
+    createBox,
+    clickAndDrag,
+    clickAndDragInterpolated
+} from './ui.js';
 
 // Create boxes
 let redbox = createBox('red', { x: 100, y: 100 })
-clickAndDrag(redbox)
-
 let bluebox = createBox('blue', { x: 200, y: 100 })
-clickAndDragInterpolated(bluebox, 0.75)
-
 let greenbox = createBox('green', { x: 300, y: 100 })
+
+// Add click and drag
+clickAndDrag(redbox)
+clickAndDragInterpolated(bluebox, 0.75)
 clickAndDragInterpolated(greenbox, 0.03)
---
+++ app/ui.js
@@ -0,0 +1,70 @@
+/**
+ * Using rxjs to animate javascript components
+ * 
+ * Author:  Anshul Kharbanda
+ * Created: 9 - 7 - 2020
+ */
+import { toggleSubscription, interpolate } from './operations';
+import { mousePos$ } from './observable';
+import { fromEvent, Subscription } from 'rxjs';
+
+// Get app
+const app = document.getElementById('app')
+
+/**
+ * Moves box to new position
+ * 
+ * @param {HTMLElement} box box to move
+ * @param {{x: float, y: float}} pos new position of box 
+ */
+export function moveBox(box, pos) {
+    box.style.left = pos.x + 'px'
+    box.style.top = pos.y + 'px'
+}
+
+/**
+ * 
+ * @param {string} color color of box
+ * @param {{x: float, y: float}} initial initial position
+ */
+export function createBox(color, initial={x: 0, y: 0}) {
+    let box = document.createElement('div')
+    box.classList.add('animated-box')
+    box.style.backgroundColor = color
+    app.appendChild(box)
+    moveBox(box, initial)
+    return box
+}
+
+/**
+ * Adds click and drag functionality to box
+ * 
+ * @param {HTMLElement} box box element
+ * 
+ * @returns {Subscription} observable subscription for click and drag
+ */
+export function clickAndDrag(box) {
+    return fromEvent(box, 'click').subscribe(
+        toggleSubscription(
+            mousePos$,
+            pos => moveBox(box, pos)
+        )
+    )
+}
+
+/**
+ * Adds interpolated click and drag functionality to box
+ * 
+ * @param {HTMLElement} box box element
+ * @param {float} alpha interpolation alpha value
+ * 
+ * @returns {Subscription} observable subscription for click and drag
+ */
+export function clickAndDragInterpolated(box, alpha) {
+    return fromEvent(box, 'click').subscribe(
+        toggleSubscription(
+            interpolate(alpha)(mousePos$),
+            pos => moveBox(box, pos)
+        )
+    )
+}
